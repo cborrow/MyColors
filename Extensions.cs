e104f3b7eb50df1f11d335380eebfee7c2a3df68
Cory Borrow <cborrow03@gmail.com>
2013-12-14 14:02:56 -0500

    Created a extensions class
    Created an extensions class.
    Added a Deflate method for the Rectangle.

---
+++ Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyColors
+{
+    public static class Extensions
+    {
+        public static Rectangle Deflate(this Rectangle r, Size amount)
+        {
+            r.Size -= amount;
+            return r;
+        }
+    }
+}
